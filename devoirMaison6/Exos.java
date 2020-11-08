15eee556c88c730942de70d35ebe01f32bb072fc
jadecrtl <jadecortial@gmail.com>
2020-11-08 16:33:57 +0100

    Exo 1 et 2 code de Gray

--- devoirMaison6/Exos.java
+++ devoirMaison6/Exos.java
@@ -1,12 +1,24 @@
 public class Exos {
 
     /* EXERCICE 1 */
-    // public static boolean[] codeGray(boolean[] b) {
-    // }
+    public static boolean[] codeGray(boolean[] b) {
+	boolean[] g = new boolean[b.length];
+	g[0]=b[0];
+	for (int i=1; i<b.length; i++) {
+	    g[i]=ouExclusif(b[i], b[i-1]);
+	}
+	return g;
+    }
 
     /* EXERCICE 2 */
-    // public static boolean[] decodeGray(boolean[] b) {
-    // }
+    public static boolean[] decodeGray(boolean[] g) {
+	boolean[] b = new boolean[g.length];
+	b[0] = g[0];
+	for (int i=1; i<g.length; i++) {
+	    b[i]=ouExclusif(g[i], b[i-1]);
+	}
+	return b;
+    }
 
     /* EXERCICE 3 */
     // public static boolean[] aux(int n, int m) {
@@ -24,9 +36,9 @@ public class Exos {
         printBooleanArray(b);
         printBooleanArray(codeGray(b));
         printBooleanArray(decodeGray(codeGray(b)));
-        printBooleanArray(aux(3, 4));
-        System.out.println(nearGray(6, 9));
-        System.out.println(nearGray(3, 5));
+        //printBooleanArray(aux(3, 4));
+        //System.out.println(nearGray(6, 9));
+        //System.out.println(nearGray(3, 5));
     }
 
     /*************************************************************/
@@ -53,5 +65,23 @@ public class Exos {
             exponent = exponent - 1;
         }
         return result;
+    }
+
+    //Je cree ma fonction representant la methode du "ou exclusif"
+    //methode du ou exclusif :
+    //00 : 0  10 : 1  01 : 1  11 : 0
+    public static boolean ouExclusif(boolean a, boolean b) {
+	    if (a == false && b == false) {
+		return false;
+	    }
+	    if (a == true && b == false) {
+		return true;
+	    }
+	    if (a == false && b == true) {
+		return true;
+	    }
+	    else {
+		return false;
+	    }
     }
 }
